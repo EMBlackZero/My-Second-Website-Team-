27878e73a8c0c3f20ddaa587c15da7742e80c35f
Warayuwach Srisamran <fetuszaza@gmail.com>
2024-02-07 10:57:19 +0700

    edit location

--- backendcar/types/generated/contentTypes.d.ts
+++ backendcar/types/generated/contentTypes.d.ts
@@ -387,6 +387,9 @@ export interface ApiBookingBooking extends Schema.CollectionType {
       'manyToOne',
       'plugin::users-permissions.user'
     >;
+    location: Attribute.Text &
+      Attribute.Required &
+      Attribute.DefaultTo<'https://maps.app.goo.gl/ymMhmqjas8LMjVtf8'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -421,7 +424,6 @@ export interface ApiCarCar extends Schema.CollectionType {
     description: Attribute.Text & Attribute.Required;
     price: Attribute.String & Attribute.Required;
     remainder: Attribute.BigInteger;
-    location: Attribute.Text & Attribute.Required;
     bookings: Attribute.Relation<
       'api::car.car',
       'oneToMany',
