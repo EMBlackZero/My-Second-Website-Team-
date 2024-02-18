a27acb2694ce5edbc48b2c36c60ad2735576387a
Warayuwach Srisamran <fetuszaza@gmail.com>
2024-02-18 13:06:34 +0700

    payment css create

--- fronendcar/src/PageAll/PaymentPage.js
+++ fronendcar/src/PageAll/PaymentPage.js
@@ -1,12 +1,41 @@
-import React , { useEffect , useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import "../CssAll/PaymentCss.css"
 
-function PaymentPage() {
-    return ( 
-        <div>หน้าจ่ายเงิน</div>
+function PaymentPage(props) {
+    return (
+        <div>
+            <div className="navbar">
+                <a href='/PublicPage' className="titel">รถเช่าผมไม่เล็กนะครับ</a>
+                <div className="titelogin">
+                    <a href="/">
+                        <h1>Profile</h1>
+                    </a>
+                    <button onClick={props.onlogout}>Logout</button>
+
+                </div>
+            </div>
+            <div className = "paytitle"> 
+                <h2>เลือกช่องทางการจ่ายเงิน</h2>
+            </div>
+
+            <div className="button-container">
+                <button class="image-button">
+                    <img src="/creditcard.png" ></img>
+                    <span>บัตรเครดิต/เดบิต/มาสเตอร์การ์ด (ชำระออนไลน์)</span>
+                </button>
+
+                <button class="image-button">
+                    <img src="/money.png" ></img>
+                    <span>เงินสด (ชำระหน้าร้าน)</span>
+                </button></div>
+
+
+        </div>
+
+
+
     );
-  }
-  
-  export default PaymentPage;
+}
