43a5feae52a9f6d7194b11b01e582de66d0c9a75
EMBlackZero <152731798+EMBlackZero@users.noreply.github.com>
2024-02-18 13:00:17 +0700

    ทำcustomการคืนรถและเช่ารถ

--- backendcar/src/api/car/controllers/car.js
+++ backendcar/src/api/car/controllers/car.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * car controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::car.car');
+module.exports = createCoreController("api::car.car", ({ strapi }) => ({
+  async increaseRemaining(ctx) {
+    const entityId = ctx.params.id;
+    try {
+      let car = await strapi.entityService.findOne("api::car.car", entityId);
+      car = await strapi.entityService.update("api::car.car", entityId, {
+        data: { remaining: (car.remaining || 0) + 1 }, // เพิ่มค่า remaining
+      });
+      ctx.body = { ok: 1, remaining: car.remaining };
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+
+  async decreaseRemaining(ctx) {
+    const entityId = ctx.params.id;
+    try {
+      let car = await strapi.entityService.findOne("api::car.car", entityId);
+      car = await strapi.entityService.update("api::car.car", entityId, {
+        data: { remaining: (car.remaining || 0) - 1 }, // ลดค่า remaining
+      });
+      ctx.body = { ok: 1, remaining: car.remaining };
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
