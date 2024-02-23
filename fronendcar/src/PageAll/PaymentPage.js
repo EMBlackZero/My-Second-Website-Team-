69670be26802578649074e19ba63582bc636d3a1
EMBlackZero <152731798+EMBlackZero@users.noreply.github.com>
2024-02-23 14:25:23 +0700

    config

--- fronendcar/src/PageAll/PaymentPage.js
+++ fronendcar/src/PageAll/PaymentPage.js
@@ -1,12 +1,54 @@
-import React , { useEffect , useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,useParams } from "react-router-dom";
+import "../CssAll/PaymentCss.css"
 
-function PaymentPage() {
-    return ( 
-        <div>หน้าจ่ายเงิน</div>
+
+function PaymentPage(props) {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const CarDetail = (id) => {
+        navigate(`/DetailsPage/${id}`);   
+    };
+    
+    return (
+        <div>
+            <div className="navbar">
+                <a href='/PublicPage' className="titel">รถเช่าผมไม่เล็กนะครับ</a>
+                <div className="titelogin">
+                    <a href="/">
+                        <h1>Profile</h1>
+                    </a>
+                    <button onClick={props.onlogout}>Logout</button>
+
+                </div>
+            </div>
+            <div>
+                <button className="buttonback" onClick={() => CarDetail(id)} >
+                    <img src="/back.png" />
+                </button>
+            </div>
+            <div className="paytitle">
+                <h2>เลือกช่องทางการจ่ายเงิน</h2>
+            </div>
+
+            <div className="button-container">
+                <button class="image-button">
+                    <img src="/creditcard.png" ></img>
+                    <span>บัตรเครดิต/เดบิต/มาสเตอร์การ์ด (ชำระออนไลน์)</span>
+                </button>
+
+                <button class="image-button">
+                    <img src="/money.png" ></img>
+                    <span>เงินสด (ชำระหน้าร้าน)</span>
+                </button></div>
+
+
+        </div>
+
+
+
     );
-  }
-  
-  export default PaymentPage;
+}
+export default PaymentPage
