8b8e0682dd59b5bec5da3eb0e4d008ec7a9e014d
EMBlackZero <152731798+EMBlackZero@users.noreply.github.com>
2024-02-25 19:35:02 +0700

    เเก้dropdow

--- fronendcar/src/PageAll/AdminNav.js
+++ fronendcar/src/PageAll/AdminNav.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function AdminNav(props) {
   return (
     <div className="navbar">
-        <a href='/AdminPage' className="titel">รถเช่าผมไม่เล็กนะครับ</a>
-        <div className="titelogin">
-          <a href="/AdminPage">
-            <h1>Admin</h1>
-          </a>
-          <Button variant="danger" onClick={props.onlogout}>Logout</Button>
-      
-        </div>
+      <a href='/AdminPage' className="titel">รถเช่าผมไม่เล็กนะครับ</a>
+      <div className="titelogin">
+        
+        <DropdownButton
+          id="admin-dropdown"
+          title="Admin"
+          className="dropdown-with-image"
+        >
+          <Dropdown.Item onClick={props.onlogout}>Logout</Dropdown.Item>
+        </DropdownButton>
       </div>
-  )
+    </div>
+  );
 }
 
-export default AdminNav
+export default AdminNav;
--- fronendcar/src/PageAll/MemberNav.js
+++ fronendcar/src/PageAll/MemberNav.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-  
+import React from "react";
+import Button from "react-bootstrap/Button";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import "../CssAll/LoginCss.css";
+
 function MemberNav(props) {
-  const username = sessionStorage.getItem('username')
+  const username = sessionStorage.getItem("username");
   return (
     <div className="navbar">
-        <a href='/PublicPage' className="titel">รถเช่าผมไม่เล็กนะครับ</a>
-        <div className="titelogin">
-          <a href="/">
-            <h1>{username}</h1>
-          </a>
-          <Button variant="danger" onClick={props.onlogout}>Logout</Button>
-          
-        </div>
+      <a href="/PublicPage" className="titel">
+        รถเช่าผมไม่เล็กนะครับ
+      </a>
+      <div className="titelogin">
+        <DropdownButton
+          id="dropdown-basic-button"
+          title={username}
+          className="dropdown-with-image"
+        >
+          <Dropdown.Item onClick={props.onlogout}>Logout</Dropdown.Item>
+        </DropdownButton>
       </div>
-  )
+    </div>
+  );
 }
 
-export default MemberNav
+export default MemberNav;
